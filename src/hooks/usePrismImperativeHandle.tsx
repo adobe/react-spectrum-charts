3de6a6b47f731f7993362a7b0bf5a37275478dd2
Marshall Peterson <mpeterson@adobe.com>
2023-10-30 16:08:55 -0600

    fix: do not use snake case on potentially localized values

--- src/hooks/usePrismImperativeHandle.tsx
+++ src/hooks/usePrismImperativeHandle.tsx
@@ -9,15 +9,14 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-
-import { toSnakeCase } from '@utils';
 import { MutableRefObject, Ref, useImperativeHandle } from 'react';
+
 import { PrismHandle } from 'types';
 import { View } from 'vega';
 
 export default function usePrismImperativeHandle(
 	forwardedRef: Ref<PrismHandle>,
-	{ chartView, title }: { chartView: MutableRefObject<View | undefined>; title?: string },
+	{ chartView, title }: { chartView: MutableRefObject<View | undefined>; title?: string }
 ) {
 	return useImperativeHandle(forwardedRef, () => ({
 		copy() {
@@ -30,13 +29,13 @@ export default function usePrismImperativeHandle(
 								const blob = await response.blob();
 								navigator.clipboard.write([new ClipboardItem({ 'image/png': blob })]).then(
 									() => resolve('Chart copied to clipboard'),
-									() => reject('Error occurred while writing to clipboard, copy to clipboard failed'),
+									() => reject('Error occurred while writing to clipboard, copy to clipboard failed')
 								);
 							} catch (error) {
 								reject('Error occurred while fetching image, copy to clipboard failed');
 							}
 						},
-						() => reject('Error occurred while converting image to URL, copy to clipboard failed'),
+						() => reject('Error occurred while converting image to URL, copy to clipboard failed')
 					);
 				} else {
 					reject("There isn't a chart to copy, copy to clipboard failed");
@@ -46,7 +45,7 @@ export default function usePrismImperativeHandle(
 		download(customFileName?: string) {
 			return new Promise<string>((resolve, reject) => {
 				if (chartView.current) {
-					const filename = `${toSnakeCase(customFileName || title || 'prism_export')}.png`;
+					const filename = `${customFileName || title || 'chart_export'}.png`;
 					chartView.current.toImageURL('png').then(
 						(url) => {
 							const link = document.createElement('a');
@@ -56,7 +55,7 @@ export default function usePrismImperativeHandle(
 							link.dispatchEvent(new MouseEvent('click'));
 							resolve(`Chart downloaded as ${filename}`);
 						},
-						() => reject('Error occurred while converting image to URL, download failed'),
+						() => reject('Error occurred while converting image to URL, download failed')
 					);
 				} else {
 					reject("There isn't a chart to download, download failed");
