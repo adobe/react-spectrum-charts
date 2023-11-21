2825e781f2ade611daa47e6a54875f825bfdc69b
Marshall Peterson <marshall.w.peterson@gmail.com>
2023-11-21 11:30:18 -0700

    feat: selected point styling for line

--- src/specBuilder/line/linePointUtils.ts
+++ src/specBuilder/line/linePointUtils.ts
@@ -1,15 +1,18 @@
-import { BACKGROUND_COLOR, DEFAULT_SYMBOL_SIZE, DEFAULT_SYMBOL_STROKE_WIDTH } from '@constants';
+import { BACKGROUND_COLOR, DEFAULT_SYMBOL_SIZE, DEFAULT_SYMBOL_STROKE_WIDTH, MARK_ID } from '@constants';
 import {
 	getColorProductionRule,
 	getHighlightOpacityValue,
 	getOpacityProductionRule,
+	hasPopover,
 } from '@specBuilder/marks/markUtils';
+import { getColorValue } from '@specBuilder/specUtils';
 import { LineSpecProps, ProductionRuleTests } from 'types';
 import { ColorValueRef, NumericValueRef, SymbolMark } from 'vega';
 
 import { LineMarkProps, getXProductionRule } from './lineUtils';
 
 const staticPointTest = (staticPoint: string) => `datum.${staticPoint} && datum.${staticPoint} === true`;
+const selectedTest = (name: string) => `${name}_selectedId && ${name}_selectedId === datum.${MARK_ID}`;
 
 /**
  * Gets the point mark for static points on a line chart.
@@ -89,6 +92,7 @@ export const getHighlightPoint = (lineProps: LineMarkProps): SymbolMark => {
 			update: {
 				fill: getHighlightPointFill(lineProps),
 				size: getHighlightPointSize(lineProps),
+				stroke: getHighlightPointStroke(lineProps),
 				strokeOpacity: getHighlightPointStrokeOpacity(lineProps),
 				strokeWidth: getHighlightPointStrokeWidth(lineProps),
 				x: getXProductionRule(scaleType, dimension),
@@ -131,16 +135,43 @@ export const getSecondaryHighlightPoint = (
  * @param lineMarkProps
  * @returns fill rule
  */
-const getHighlightPointFill = ({
+export const getHighlightPointFill = ({
+	children,
 	color,
 	colorScheme,
+	name,
 	staticPoint,
 }: LineMarkProps): ProductionRuleTests<ColorValueRef> => {
-	const fillRules: ProductionRuleTests<ColorValueRef> = [{ signal: BACKGROUND_COLOR }];
+	const fillRules: ProductionRuleTests<ColorValueRef> = [];
 	if (staticPoint) {
-		fillRules.unshift({ test: staticPointTest(staticPoint), ...getColorProductionRule(color, colorScheme) });
+		fillRules.push({ test: staticPointTest(staticPoint), ...getColorProductionRule(color, colorScheme) });
 	}
-	return fillRules;
+	if (hasPopover(children)) {
+		fillRules.push({ test: selectedTest(name), ...getColorProductionRule(color, colorScheme) });
+	}
+	return [...fillRules, { signal: BACKGROUND_COLOR }];
+};
+
+/**
+ * gets the stroke color for the highlighted point
+ * @param lineMarkProps
+ * @returns stroke rule
+ */
+export const getHighlightPointStroke = ({
+	children,
+	color,
+	colorScheme,
+	name,
+	staticPoint,
+}: LineMarkProps): ProductionRuleTests<ColorValueRef> => {
+	const strokeRules: ProductionRuleTests<ColorValueRef> = [];
+	if (staticPoint) {
+		strokeRules.push({ test: staticPointTest(staticPoint), ...getColorProductionRule(color, colorScheme) });
+	}
+	if (hasPopover(children)) {
+		strokeRules.push({ test: selectedTest(name), signal: BACKGROUND_COLOR });
+	}
+	return [...strokeRules, getColorProductionRule(color, colorScheme)];
 };
 
 /**
@@ -148,19 +179,19 @@ const getHighlightPointFill = ({
  * @param lineMarkProps
  * @returns stroke opacity rule
  */
-const getHighlightPointStrokeOpacity = ({
+export const getHighlightPointStrokeOpacity = ({
 	opacity,
 	staticPoint,
 }: LineMarkProps): ProductionRuleTests<NumericValueRef> => {
 	const baseOpacityRule = getOpacityProductionRule(opacity);
-	const strokeOpacityRules: ProductionRuleTests<NumericValueRef> = [baseOpacityRule];
+	const strokeOpacityRules: ProductionRuleTests<NumericValueRef> = [];
 	if (staticPoint) {
-		strokeOpacityRules.unshift({
+		strokeOpacityRules.push({
 			test: staticPointTest(staticPoint),
 			...getHighlightOpacityValue(baseOpacityRule),
 		});
 	}
-	return strokeOpacityRules;
+	return [...strokeOpacityRules, baseOpacityRule];
 };
 
 /**
@@ -168,16 +199,16 @@ const getHighlightPointStrokeOpacity = ({
  * @param lineMarkProps
  * @returns size rule
  */
-const getHighlightPointSize = ({ staticPoint }: LineMarkProps): ProductionRuleTests<NumericValueRef> => {
-	const sizeRules: ProductionRuleTests<NumericValueRef> = [{ value: DEFAULT_SYMBOL_SIZE }];
+export const getHighlightPointSize = ({ staticPoint }: LineMarkProps): ProductionRuleTests<NumericValueRef> => {
+	const sizeRules: ProductionRuleTests<NumericValueRef> = [];
 	if (staticPoint) {
-		sizeRules.unshift({
+		sizeRules.push({
 			// if this is a static point, reduce the size since we are increasing the stroke width
 			test: staticPointTest(staticPoint),
 			value: 64,
 		});
 	}
-	return sizeRules;
+	return [...sizeRules, { value: DEFAULT_SYMBOL_SIZE }];
 };
 
 /**
@@ -185,14 +216,41 @@ const getHighlightPointSize = ({ staticPoint }: LineMarkProps): ProductionRuleTe
  * @param lineMarkProps
  * @returns stroke width rule
  */
-const getHighlightPointStrokeWidth = ({ staticPoint }: LineMarkProps): ProductionRuleTests<NumericValueRef> => {
-	const strokeWidthRules: ProductionRuleTests<NumericValueRef> = [{ value: DEFAULT_SYMBOL_STROKE_WIDTH }];
+export const getHighlightPointStrokeWidth = ({ staticPoint }: LineMarkProps): ProductionRuleTests<NumericValueRef> => {
+	const strokeWidthRules: ProductionRuleTests<NumericValueRef> = [];
 	if (staticPoint) {
-		strokeWidthRules.unshift({
+		strokeWidthRules.push({
 			// if the point is static, increase the stroke width
 			test: staticPointTest(staticPoint),
 			value: 6,
 		});
 	}
-	return strokeWidthRules;
+	return [...strokeWidthRules, { value: DEFAULT_SYMBOL_STROKE_WIDTH }];
+};
+
+/**
+ * Gets point that is used for the selection ring.
+ * @param lineMarkProps
+ * @returns SymbolMark
+ */
+export const getSelectRingPoint = (lineProps: LineMarkProps): SymbolMark => {
+	const { colorScheme, dimension, metric, name, scaleType } = lineProps;
+	return {
+		name: `${name}_pointSelectRing`,
+		type: 'symbol',
+		from: { data: `${name}_highlightedData` },
+		interactive: false,
+		encode: {
+			enter: {
+				y: { scale: 'yLinear', field: metric },
+				fill: { signal: BACKGROUND_COLOR },
+				stroke: { value: getColorValue('static-blue', colorScheme) },
+			},
+			update: {
+				size: [{ test: selectedTest(name), value: 196 }, { value: 0 }],
+				strokeWidth: [{ test: selectedTest(name), value: DEFAULT_SYMBOL_STROKE_WIDTH }, { value: 0 }],
+				x: getXProductionRule(scaleType, dimension),
+			},
+		},
+	};
 };
