31caeddb24d457a3a072f1863392df90a01b8835
Judd2000 <njudd.mail@gmail.com>
2024-02-12 10:48:49 -0700

    Byu/Gtonkinson fix tests

--- src/stories/components/BigNumber/BigNumber.test.tsx
+++ src/stories/components/BigNumber/BigNumber.test.tsx
@@ -106,20 +106,17 @@ describe('BigNumber', () => {
 		test('Chart with multiple BigNumbers only displays first', async () => {
 			render(
 				<Chart data={simpleSparklineData}>
-					<BigNumber dataKey='y' orientation="horizontal" label="test">
-						<Icon data-testid="first-icon">
-							<svg></svg>
-						</Icon>
+					<BigNumber dataKey='y' orientation="horizontal" label="test" icon={<Icon data-testid="first-icon">
+					<svg></svg>
+		</Icon>}>
 					</BigNumber>
-					<BigNumber dataKey='y' orientation="horizontal" label="test">
-						<Icon data-testid="second-icon">
-							<svg></svg>
-						</Icon>
+					<BigNumber dataKey='y' orientation="horizontal" label="test" icon={<Icon data-testid="second-icon">
+						<svg></svg>
+					</Icon>}>
 					</BigNumber>
-					<BigNumber dataKey='y' orientation="horizontal" label="test">
-						<Icon data-testid="third-icon">
-							<svg></svg>
-						</Icon>
+					<BigNumber dataKey='y' orientation="horizontal" label="test" icon={<Icon data-testid="third-icon">
+						<svg></svg>
+					</Icon>}>
 					</BigNumber>
 				</Chart>
 			);
