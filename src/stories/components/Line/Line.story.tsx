4dfb224d6dcb6f026c14c0a8bb80cfeb92fbc3b1
Marshall Peterson <marshall.w.peterson@gmail.com>
2024-03-05 15:58:00 -0700

    chore: remove debug

--- src/stories/components/Line/Line.story.tsx
+++ src/stories/components/Line/Line.story.tsx
@@ -61,7 +61,7 @@ const defaultChartProps: ChartProps = { data: workspaceTrendsData, minWidth: 400
 const BasicLineStory: StoryFn<typeof Line> = (args): ReactElement => {
 	const chartProps = useChartProps(defaultChartProps);
 	return (
-		<Chart {...chartProps} debug>
+		<Chart {...chartProps}>
 			<Line {...args} />
 			<Legend lineWidth={{ value: 0 }} />
 		</Chart>
