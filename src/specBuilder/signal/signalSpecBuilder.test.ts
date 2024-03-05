96117c848f0a62893b88d3393a77ade6a261c15e
Marshall Peterson <marshall.w.peterson@gmail.com>
2024-03-05 16:17:12 -0700

    test: add coverage

--- src/specBuilder/signal/signalSpecBuilder.test.ts
+++ src/specBuilder/signal/signalSpecBuilder.test.ts
@@ -10,6 +10,13 @@
  * governing permissions and limitations under the License.
  */
 import { Signal } from 'vega';
+import { addHighlightedItemSignalEvents, addHighlightedSeriesSignalEvents } from './signalSpecBuilder';
+import {
+	defaultHighlightedItemSignal,
+	defaultHighlightedSeriesSignal,
+	defaultSignals,
+} from '@specBuilder/specTestUtils';
+import { HIGHLIGHTED_ITEM, HIGHLIGHTED_SERIES } from '@constants';
 
 export const defaultHighlightSignal: Signal = {
 	name: 'highlightedSeries',
@@ -22,3 +29,45 @@ export const defaultHighlightSignal: Signal = {
 		{ events: '@legend0_legendEntry:mouseout', update: '""' },
 	],
 };
+
+describe('signalSpecBuilder', () => {
+	let signals: Signal[];
+	beforeEach(() => {
+		signals = JSON.parse(JSON.stringify(defaultSignals));
+	});
+	describe('addHighlightedItemSignalEvents()', () => {
+		test('should add on events', () => {
+			addHighlightedItemSignalEvents(signals, 'line0');
+			expect(signals).toHaveLength(2);
+			expect(signals[0]).toHaveProperty('name', HIGHLIGHTED_ITEM);
+			expect(signals[0].on).toHaveLength(2);
+			expect(signals[0]?.on?.[0]).toHaveProperty('events', '@line0:mouseover');
+			expect(signals[0]?.on?.[1]).toHaveProperty('events', '@line0:mouseout');
+			expect(signals[1].on).toBeUndefined();
+		});
+		test('should not do anything if the highlight signal is not found', () => {
+			const signals = JSON.parse(JSON.stringify([defaultHighlightedSeriesSignal]));
+			const signalsCopy = JSON.parse(JSON.stringify(signals));
+			addHighlightedItemSignalEvents(signals, 'line0');
+			expect(signals).toEqual(signalsCopy);
+		});
+	});
+
+	describe('addHighlightedSeriesSignalEvents()', () => {
+		test('should add on events', () => {
+			addHighlightedSeriesSignalEvents(signals, 'line0');
+			expect(signals).toHaveLength(2);
+			expect(signals[0].on).toBeUndefined();
+			expect(signals[1]).toHaveProperty('name', HIGHLIGHTED_SERIES);
+			expect(signals[1].on).toHaveLength(2);
+			expect(signals[1]?.on?.[0]).toHaveProperty('events', '@line0:mouseover');
+			expect(signals[1]?.on?.[1]).toHaveProperty('events', '@line0:mouseout');
+		});
+		test('should not do anything if the highlight signal is not found', () => {
+			const signals = JSON.parse(JSON.stringify([defaultHighlightedItemSignal]));
+			const signalsCopy = JSON.parse(JSON.stringify(signals));
+			addHighlightedSeriesSignalEvents(signals, 'line0');
+			expect(signals).toEqual(signalsCopy);
+		});
+	});
+});
