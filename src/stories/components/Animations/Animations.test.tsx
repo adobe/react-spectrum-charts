c7cc0a1c71d714b67f33721bd4480f58a395659e
Gideon Tonkinson <gmtonkinson@gmail.com>
2024-03-08 11:19:37 -0700

    Test: Add testing for the animation stories

--- src/stories/components/Animations/Animations.test.tsx
+++ src/stories/components/Animations/Animations.test.tsx
@@ -1,3 +1,62 @@
+import { findChart, render } from '@test-utils';
+
+import {
+	AreaSwitch,
+	AreaZero,
+	BarSwitch,
+	BarZero,
+	DodgedBarSwitch,
+	DodgedBarZero,
+	SingleLineSwitch,
+	SingleLineZero,
+} from './Animations.story';
+
 describe('Animations', () => {
-    
+	test('Area Switch renders properly', async () => {
+		render(<AreaSwitch {...AreaSwitch.args} />);
+		const chart = await findChart();
+		expect(chart).toBeInTheDocument();
+	});
+
+	test('Area Zero renders properly', async () => {
+		render(<AreaZero {...AreaZero.args} />);
+		const chart = await findChart();
+		expect(chart).toBeInTheDocument();
+	});
+
+	test('Single Line Switch renders properly', async () => {
+		render(<SingleLineSwitch {...SingleLineSwitch.args} />);
+		const chart = await findChart();
+		expect(chart).toBeInTheDocument();
+	});
+
+	test('Single Line Zero renders properly', async () => {
+		render(<SingleLineZero {...SingleLineZero.args} />);
+		const chart = await findChart();
+		expect(chart).toBeInTheDocument();
+	});
+
+	test('Bar Switch renders properly', async () => {
+		render(<BarSwitch {...BarSwitch.args} />);
+		const chart = await findChart();
+		expect(chart).toBeInTheDocument();
+	});
+
+	test('Bar Zero renders properly', async () => {
+		render(<BarZero {...BarZero.args} />);
+		const chart = await findChart();
+		expect(chart).toBeInTheDocument();
+	});
+
+	test('Dodged Bar Switch renders properly', async () => {
+		render(<DodgedBarSwitch {...DodgedBarSwitch.args} />);
+		const chart = await findChart();
+		expect(chart).toBeInTheDocument();
+	});
+
+	test('Dodged Bar Zero renders properly', async () => {
+		render(<DodgedBarZero {...DodgedBarZero.args} />);
+		const chart = await findChart();
+		expect(chart).toBeInTheDocument();
+	});
 });
