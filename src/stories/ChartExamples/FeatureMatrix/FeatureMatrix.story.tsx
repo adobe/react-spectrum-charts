71f1c598c978fc59ff37c72e1092039c5a1e314c
olsonjb <jbolson98@gmail.com>
2024-04-09 10:38:05 -0600

    Add story to showcase excludeDataKeys

--- src/stories/ChartExamples/FeatureMatrix/FeatureMatrix.story.tsx
+++ src/stories/ChartExamples/FeatureMatrix/FeatureMatrix.story.tsx
@@ -12,11 +12,11 @@
 import { ReactElement } from 'react';
 
 import useChartProps from '@hooks/useChartProps';
-import { Axis, Chart, Legend, Scatter, ScatterPath, Trendline, TrendlineAnnotation, TrendlineProps } from '@rsc';
+import { Axis, categorical12, Chart, ChartTooltip, Legend, Scatter, ScatterPath, Trendline, TrendlineAnnotation, TrendlineProps } from '@rsc';
 import { StoryFn } from '@storybook/react';
 import { bindWithProps } from '@test-utils';
 
-import { basicFeatureMatrixData, multipleSegmentFeatureMatrixData, timeCompareFeatureMatrixData } from './data';
+import { basicFeatureMatrixData, multipleSegmentFeatureMatrixData, timeCompareFeatureMatrixData, topEventsFeatureMatrixData } from './data';
 
 export default {
 	title: 'RSC/Chart/Examples',
@@ -98,6 +98,33 @@ const TimeCompareFeatureMatrixStory: StoryFn<typeof Chart> = (args): ReactElemen
 	);
 };
 
+const EventOverlayFeatureMatrixStory: StoryFn<typeof Chart> = (args): ReactElement => {
+	const chartProps = useChartProps(args);
+
+	return (
+		<Chart {...chartProps}>
+			<Axis position="bottom" ticks grid title="Percentage of daily users (DAU)" labelFormat="percentage" />
+			<Axis position="left" ticks grid title="Average number of times per day" />
+			<Scatter dimension="dauPercent" metric="countAvg" color="segment" opacity="segment">
+				<ChartTooltip excludeDataKeys={['isTopItem']} />
+				<Trendline {...trendlineProps} excludeDataKeys={['isTopItem']} displayOnHover orientation="horizontal">
+					<TrendlineAnnotation prefix="Median times" numberFormat=".3" />
+				</Trendline>
+				<Trendline {...trendlineProps} excludeDataKeys={['isTopItem']} displayOnHover orientation="vertical" >
+					<TrendlineAnnotation prefix="Median %DAU" numberFormat=".2%" dimensionValue="start" />
+				</Trendline>
+				<Trendline {...trendlineProps} excludeDataKeys={['isSegmentData']} orientation="horizontal" color='gray-700' opacity={0.7}>
+					<TrendlineAnnotation prefix="Top events median times" numberFormat=".3" />
+				</Trendline>
+				<Trendline {...trendlineProps} excludeDataKeys={['isSegmentData']} orientation="vertical" color='gray-700' opacity={0.7} >
+					<TrendlineAnnotation prefix="Top events median %DAU" numberFormat=".2%" />
+				</Trendline>
+			</Scatter>
+			<Legend position="bottom" highlight hiddenEntries={['top-events-overlay']} />
+		</Chart>
+	);
+};
+
 const FeatureMatrix = bindWithProps(FeatureMatrixStory);
 FeatureMatrix.args = {
 	width: 'auto',
@@ -125,4 +152,14 @@ TimeCompareFeatureMatrix.args = {
 	data: timeCompareFeatureMatrixData,
 };
 
-export { FeatureMatrix, MultipleSegmentFeatureMatrix, TimeCompareFeatureMatrix };
+const EventOverlayFeatureMatrix = bindWithProps(EventOverlayFeatureMatrixStory);
+EventOverlayFeatureMatrix.args = {
+	width: 'auto',
+	maxWidth: 850,
+	height: 500,
+	colors: ['gray-400', ...categorical12],
+	opacities: [0.5, 1, 1, 1],
+	data: topEventsFeatureMatrixData,
+};
+
+export { FeatureMatrix, MultipleSegmentFeatureMatrix, TimeCompareFeatureMatrix, EventOverlayFeatureMatrix };
