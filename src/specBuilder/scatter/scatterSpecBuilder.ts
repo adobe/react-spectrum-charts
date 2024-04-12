9bf174c40c6ed1a0fb24d0de5578c21edd92b251
Gideon Tonkinson <gmtonkinson@gmail.com>
2024-04-12 11:38:28 -0600

    remove more todos

--- src/specBuilder/scatter/scatterSpecBuilder.ts
+++ src/specBuilder/scatter/scatterSpecBuilder.ts
@@ -167,8 +167,7 @@ export const setScales = produce<Scale[], [ScatterSpecProps]>((scales, props) =>
 		opacity,
 		size,
 	} = props;
-	// if animations are enabled, add Opacity animation scales to spec
-	// TODO: add tests
+	// if animations are enabled, add Opacity animation scales to spec	
 	if (animations) {
 		addRscAnimationScales(scales);
 	}
