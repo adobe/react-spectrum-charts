2d97cba2ea5958014140f9a2890b2989b1a43875
Gideon Tonkinson <gmtonkinson@gmail.com>
2024-04-12 11:36:44 -0600

    remove warns

--- src/stories/components/Area/Area.test.tsx
+++ src/stories/components/Area/Area.test.tsx
@@ -95,7 +95,6 @@ describe('Area', () => {
 	});
 
 	test('Area with Popover renders properly', async () => {
-		console.warn('Popover args are', Popover.args);
 		render(<Popover {...Popover.args} />);
 		const chart = await findChart();
 		expect(chart).toBeInTheDocument();
