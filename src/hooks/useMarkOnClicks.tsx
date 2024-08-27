093aa9d4fdd096d488bdd02ad8a73bf4fe28f928
adjenkin <adjenkin@adobe.com>
2024-08-26 23:52:59 -0700

    defined mark click hook

---
+++ src/hooks/useMarkOnClicks.tsx
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2023 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+import { createElement, useMemo } from 'react';
+
+import { getAllElements } from '@utils';
+
+import { Chart } from '../Chart';
+import { Bar } from '../components/Bar';
+import { BarElement, ChartChildElement, Datum, MarkClickHandler } from '../types';
+
+type MappedMarkElement = {name: string, element: BarElement}
+
+export type MarkDetail = {
+	name: string;
+	callback: MarkClickHandler;
+    onClick: (datum: Datum) => void;
+};
+
+export default function useMarkOnClicks(children: ChartChildElement[]): MarkDetail[] {
+	const markElements = useMemo(
+		() => getAllElements(createElement(Chart, { data: [] }, children), Bar, []) as MappedMarkElement[],
+		[children]
+	);
+
+	return useMemo(
+		() =>
+			markElements
+				.filter((mark) => mark.element.props.children)
+				.map((mark) => ({
+					name: mark.name,
+                    callback: mark.element.props
+				})) as MarkDetail[],
+		[markElements]
+	);
+}
