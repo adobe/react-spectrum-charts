0996ae962202eb07b58a0da8eaf474a1e20e7ce5
Marshall Peterson <marshall.w.peterson@gmail.com>
2024-10-11 13:55:42 -0600

    test: the build

--- src/specBuilder/bar/barUtils.ts
+++ src/specBuilder/bar/barUtils.ts
@@ -43,6 +43,14 @@ import {
 import { BarSpecProps, Orientation } from '../../types';
 import { getTrellisProperties, isTrellised } from './trellisedBarUtils';
 
+export const myNewFunction = (sentiment?: 'positive' | 'false') => {
+	const mySentiment = sentiment ?? 'positive';
+
+	if (mySentiment === 'positive') {
+		return 'I am happy';
+	}
+	return 'I am sad';
+};
 /**
  * checks to see if the bar is faceted in the stacked and dodged dimensions
  * @param color
