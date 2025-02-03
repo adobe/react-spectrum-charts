ac90c540b544f49bbfad19043a848a24b7a5b50b
Marshall Peterson <marshall.w.peterson@gmail.com>
2025-02-03 14:19:57 -0700

    refactor: de-react-ify trendlines

--- src/specBuilder/trendline/trendlineTestUtils.ts
+++ src/specBuilder/trendline/trendlineTestUtils.ts
@@ -9,50 +9,9 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-import { createElement } from 'react';
-
-import { Trendline } from '@components/Trendline';
 import { DEFAULT_COLOR, DEFAULT_COLOR_SCHEME, DEFAULT_METRIC, DEFAULT_TIME_DIMENSION, MARK_ID } from '@constants';
 
-import { LineSpecOptions, LineSpecProps, TrendlineSpecOptions, TrendlineSpecProps } from '../../types';
-
-export const defaultLineProps: LineSpecProps = {
-	children: [createElement(Trendline, { method: 'average' })],
-	color: DEFAULT_COLOR,
-	colorScheme: DEFAULT_COLOR_SCHEME,
-	dimension: DEFAULT_TIME_DIMENSION,
-	idKey: MARK_ID,
-	index: 0,
-	lineType: { value: 'solid' },
-	markType: 'line',
-	metric: DEFAULT_METRIC,
-	name: 'line0',
-	opacity: { value: 1 },
-	scaleType: 'time',
-	interactiveMarkName: undefined,
-	popoverMarkName: undefined,
-};
-
-export const defaultTrendlineProps: TrendlineSpecProps = {
-	children: [],
-	colorScheme: DEFAULT_COLOR_SCHEME,
-	dimensionExtent: [null, null],
-	dimensionRange: [null, null],
-	dimensionScaleType: 'time',
-	displayOnHover: false,
-	highlightRawPoint: false,
-	isDimensionNormalized: false,
-	lineType: 'dashed',
-	lineWidth: 'M',
-	method: 'average',
-	metric: DEFAULT_METRIC,
-	name: 'line0Trendline0',
-	opacity: 1,
-	orientation: 'horizontal',
-	trendlineColor: DEFAULT_COLOR,
-	trendlineDimension: DEFAULT_TIME_DIMENSION,
-	trendlineMetric: DEFAULT_METRIC,
-};
+import { LineSpecOptions, TrendlineSpecOptions } from '../../types';
 
 export const defaultLineOptions: LineSpecOptions = {
 	chartPopovers: [],
