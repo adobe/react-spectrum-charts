58a2ea5f66d4a25b23263f4e519372d17c18ef00
Marshall Peterson <marshall.w.peterson@gmail.com>
2025-02-06 15:30:47 -0700

    test: add test coverage for rscToSbAdapter

---
+++ src/rscToSbAdapter/bigNumberAdapter.test.ts
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2025 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+import { createElement } from 'react';
+
+import { Line } from '@components/Line';
+
+import { BigNumberProps } from '../types';
+import { getBigNumberOptions } from './bigNumberAdapter';
+
+const basicBigNumberProps: BigNumberProps = {
+	dataKey: 'test',
+	label: 'Hello world',
+	orientation: 'horizontal',
+};
+
+describe('getBigNumberOptions()', () => {
+	it('should return all basic options', () => {
+		const options = getBigNumberOptions(basicBigNumberProps);
+		expect(options.markType).toBe('bigNumber');
+		expect(options.lines).toHaveLength(0);
+	});
+	it('should convert line children to bigNumberLines array', () => {
+		const options = getBigNumberOptions({ ...basicBigNumberProps, children: [createElement(Line)] });
+		expect(options.lines).toHaveLength(1);
+	});
+	it('should pass through included props', () => {
+		const options = getBigNumberOptions({ ...basicBigNumberProps, numberFormat: 'currency' });
+		expect(options).toHaveProperty('numberFormat', 'currency');
+	});
+	it('should not add props that are not provided', () => {
+		const options = getBigNumberOptions(basicBigNumberProps);
+		expect(options).not.toHaveProperty('numberFormat');
+	});
+});
---
+++ src/rscToSbAdapter/lineAdapter.test.ts
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2025 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+import { createElement } from 'react';
+
+import { ChartPopover } from '@components/ChartPopover';
+import { ChartTooltip } from '@components/ChartTooltip';
+import { MetricRange } from '@components/MetricRange';
+import { Trendline } from '@components/Trendline';
+import { DEFAULT_COLOR } from '@constants';
+
+import { getLineOptions } from './lineAdapter';
+
+describe('getLineOptions()', () => {
+	it('should return all basic options', () => {
+		const options = getLineOptions({});
+		expect(options.markType).toBe('line');
+		expect(options.hasOnClick).toBe(false);
+		expect(options.chartPopovers).toHaveLength(0);
+		expect(options.chartTooltips).toHaveLength(0);
+		expect(options.metricRanges).toHaveLength(0);
+		expect(options.trendlines).toHaveLength(0);
+	});
+	it('should convert popover children to chartPopovers array', () => {
+		const options = getLineOptions({ children: [createElement(ChartPopover)] });
+		expect(options.chartPopovers).toHaveLength(1);
+	});
+	it('should convert tooltip children to chartTooltips array', () => {
+		const options = getLineOptions({ children: [createElement(ChartTooltip)] });
+		expect(options.chartTooltips).toHaveLength(1);
+	});
+	it('should convert metric range children to metricRanges array', () => {
+		const options = getLineOptions({ children: [createElement(MetricRange)] });
+		expect(options.metricRanges).toHaveLength(1);
+	});
+	it('should convert trendline children to trendlines array', () => {
+		const options = getLineOptions({ children: [createElement(Trendline)] });
+		expect(options.trendlines).toHaveLength(1);
+	});
+	test('should set hasOnClick to true if onClickProp exists and is not undefined', () => {
+		expect(getLineOptions({ onClick: () => {} }).hasOnClick).toBe(true);
+		expect(getLineOptions({ onClick: undefined }).hasOnClick).toBe(false);
+	});
+	it('should pass through included props', () => {
+		const options = getLineOptions({ color: DEFAULT_COLOR });
+		expect(options).toHaveProperty('color', DEFAULT_COLOR);
+	});
+	it('should not add props that are not provided', () => {
+		const options = getLineOptions({});
+		expect(options).not.toHaveProperty('color');
+	});
+});
