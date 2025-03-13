772a86627988b0cb4ce6dbcbb1ee73016b12289b
Marshall Peterson <marshall.w.peterson@gmail.com>
2025-03-13 17:45:10 -0600

    build: fix import path

--- packages/themes/src/sequentialColorPalette.ts
+++ packages/themes/src/sequentialColorPalette.ts
@@ -9,7 +9,7 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-import { spectrumColors } from '.';
+import { spectrumColors } from './spectrumColors';
 
 const colors = spectrumColors.light;
 
