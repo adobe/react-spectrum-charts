fc95c3bf9db0433cf0ecb325e5a1c9d8739d776a
rencherga <rencher.grant@gmail.com>
2025-03-26 17:26:30 -0600

    Linting fix

--- src/specBuilder/bullet/bulletMarkUtils.test.ts
+++ src/specBuilder/bullet/bulletMarkUtils.test.ts
@@ -9,7 +9,6 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-import { scaleType } from 'vega-lite/build/src/compile/scale/type';
 import {
 	getBulletData,
 	getBulletMarkLabel,
