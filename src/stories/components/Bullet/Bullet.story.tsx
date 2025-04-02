66f88ad20457696c50694c805b21578e432ed0fe
Spencer <spenjnb@gmail.com>
2025-04-01 20:53:19 -0600

    Add bullet data utility tests

--- src/stories/components/Bullet/Bullet.story.tsx
+++ src/stories/components/Bullet/Bullet.story.tsx
@@ -62,6 +62,7 @@ Basic.args = {
 	color: 'blue-900',
 	direction: 'column',
 	numberFormat: '$,.2f',
+	thresholdValues: basicThresholdsData,
 	showTarget: true,
 	showTargetValue: false,
 	labelPosition: 'top',
