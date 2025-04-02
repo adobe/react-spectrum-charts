5bb6b4801932ebc9bdd380a69e4484eead0548f2
Spencer <spenjnb@gmail.com>
2025-04-01 18:30:55 -0600

    Refactor bullet data handling by adding produce, Add helper data functions and update tests

---
+++ src/specBuilder/bullet/bulletDataUtils.ts
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2025 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+import { TABLE } from '@constants';
+import { getTableData } from '@specBuilder/data/dataUtils';
+import { Data, FormulaTransform, ValuesData } from 'vega';
+
+import { BulletSpecProps } from '../../types';
+
+/**
+ * Retrieves the bullet table data from the provided data array.
+ * If it doesn't exist, creates and pushes a new one.
+ * @param data The data array.
+ * @returns The bullet table data.
+ */
+export const getBulletTableData = (data: Data[]): ValuesData => {
+	let tableData = getTableData(data);
+	if (!tableData) {
+		tableData = {
+			name: TABLE,
+			values: [],
+			transform: [],
+		};
+		data.push(tableData);
+	}
+	return tableData;
+};
+
+/**
+ * Generates the necessary formula transforms for the bullet chart.
+ * It calculates the xPaddingForTarget and, if in flexible scale mode, adds the flexibleScaleValue.
+ * @param props The bullet spec properties.
+ * @returns An array of formula transforms.
+ */
+export const getBulletTransforms = (props: BulletSpecProps): FormulaTransform[] => {
+	const transforms: FormulaTransform[] = [
+		{
+			type: 'formula',
+			expr: `isValid(datum.${props.target}) ? round(datum.${props.target} * 1.05) : 0`,
+			as: 'xPaddingForTarget',
+		},
+	];
+
+	if (props.scaleType === 'flexible') {
+		transforms.push({
+			type: 'formula',
+			expr: `${props.maxScaleValue}`,
+			as: 'flexibleScaleValue',
+		});
+	}
+	return transforms;
+};
