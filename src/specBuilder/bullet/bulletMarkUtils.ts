c41eaa9d844cf911e404ed28bf17f32028804c84
Spencer <spenjnb@gmail.com>
2025-04-01 21:21:47 -0600

    Fix code smell

--- src/specBuilder/bullet/bulletMarkUtils.ts
+++ src/specBuilder/bullet/bulletMarkUtils.ts
@@ -103,7 +103,7 @@ function getBulletGroupHeightExpression(props: BulletSpecProps): string {
 
 export const addData = produce<Data[], [BulletSpecProps]>((data, props) => {
 	const tableData = getBulletTableData(data);
-	tableData.values = props.children && props.children?.length ? props.children : [];
+	tableData.values = props.children?.length ? props.children : [];
 	tableData.transform = getBulletTransforms(props);
 });
 
