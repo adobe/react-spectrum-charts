254f574e98aac3f1ec296e7264526dfdde37fb40
Connor Lamoureux <29240999+c-lamoureux@users.noreply.github.com>
2025-04-28 13:26:32 -0600

    chore: fixed bar test data name

--- packages/react-spectrum-charts/src/stories/components/Bar/data.ts
+++ packages/react-spectrum-charts/src/stories/components/Bar/data.ts
@@ -92,7 +92,7 @@ export const stackedBarDataWithUTC = [
 	},
 ];
 
-export const barSeriesDataTwoSeries = [
+export const barDataTwoSeries = [
 	{ browser: 'Chrome', value: 5, operatingSystem: 'Windows', order: 2, percentLabel: '50%' },
 	{ browser: 'Chrome', value: 3, operatingSystem: 'Mac', order: 1, percentLabel: '30%' },
 	{ browser: 'Firefox', value: 3, operatingSystem: 'Windows', order: 2, percentLabel: '42.6%' },
@@ -102,7 +102,7 @@ export const barSeriesDataTwoSeries = [
 ];
 
 export const barSeriesData = [
-	...barSeriesDataTwoSeries,
+	...barDataTwoSeries,
 	{ browser: 'Chrome', value: 2, operatingSystem: 'Other', order: 0, percentLabel: '20%' },
 	{ browser: 'Firefox', value: 1, operatingSystem: 'Other', order: 0, percentLabel: '14.3%' },
 	{ browser: 'Safari', value: 1, operatingSystem: 'Other', order: 0, percentLabel: '25%' },
