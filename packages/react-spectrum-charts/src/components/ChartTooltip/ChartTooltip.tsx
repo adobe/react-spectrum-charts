c704a9cec159b22da5c7e5cdce45d307d76c7e96
marshallpete <marshall.w.peterson@gmail.com>
2025-11-05 16:25:00 -0700

    fix: lint errors

--- packages/react-spectrum-charts/src/components/ChartTooltip/ChartTooltip.tsx
+++ packages/react-spectrum-charts/src/components/ChartTooltip/ChartTooltip.tsx
@@ -9,11 +9,12 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { FC } from 'react';
 
 import { ChartTooltipProps } from '../../types';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const ChartTooltip: FC<ChartTooltipProps> = ({
   children,
   excludeDataKeys,
