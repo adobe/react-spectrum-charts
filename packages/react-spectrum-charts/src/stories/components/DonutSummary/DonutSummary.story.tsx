eeb344a88c38a9218ec29a1592a469abe0942b72
Connor Lamoureux <29240999+c-lamoureux@users.noreply.github.com>
2025-11-24 09:56:23 -0700

    feat: donut segmentLabels S2 styles

--- packages/react-spectrum-charts/src/stories/components/DonutSummary/DonutSummary.story.tsx
+++ packages/react-spectrum-charts/src/stories/components/DonutSummary/DonutSummary.story.tsx
@@ -89,4 +89,26 @@ Responsive.args = {
   label: 'Visitors',
 };
 
+<<<<<<< HEAD
 export { Basic, NoLabel, NumberFormat, Responsive };
+=======
+const S2Story: StoryFn<DonutSummaryProps & { width?: number; height?: number }> = (args): ReactElement => {
+  const { width, height, ...donutSummaryProps } = args;
+  const chartProps = useChartProps({ ...defaultChartProps, width: width ?? 350, height: height ?? 350, S2: true });
+  return (
+    <Chart {...chartProps}>
+      <Donut metric="count" color="browser">
+        <DonutSummary {...donutSummaryProps} />
+      </Donut>
+    </Chart>
+  );
+};
+
+const Spectrum2 = bindWithProps(S2Story);
+Spectrum2.args = {
+  label: 'Visitors',
+  numberFormat: 'shortNumber',
+};
+
+export { Basic, NoLabel, NumberFormat, Responsive, Spectrum2 };
+>>>>>>> 5641c38f (feat: donut segmentLabels S2 styles)
