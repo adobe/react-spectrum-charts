71c9bdf199ca9a41abd3b25a805132b53130863a
Lucas Gandola <lgandola@adobe.com>
2026-02-03 09:42:33 -0700

    enable axis label tooltip on time-based axes

---
+++ packages/react-spectrum-charts/src/stories/components/Axis/data.ts
@@ -0,0 +1,9 @@
+export const weekTimeData = [
+  { datetime: 1667890800000, downloads: 12 }, // Wed, Nov 8, 2022
+  { datetime: 1667977200000, downloads: 9 },  // Thu
+  { datetime: 1668063600000, downloads: 15 }, // Fri
+  { datetime: 1668150000000, downloads: 7 },  // Sat
+  { datetime: 1668236400000, downloads: 11 }, // Sun
+  { datetime: 1668322800000, downloads: 14 }, // Mon
+  { datetime: 1668409200000, downloads: 10 }, // Tue
+];
