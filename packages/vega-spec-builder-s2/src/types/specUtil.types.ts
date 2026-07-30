a2d6a8da0e5130836fe7a8ec1437394e849d6ce8
Tanu Garg <tanugarg86@gmail.com>
2026-07-30 12:46:14 -0600

    fix: fix type

--- packages/vega-spec-builder-s2/src/types/specUtil.types.ts
+++ packages/vega-spec-builder-s2/src/types/specUtil.types.ts
@@ -73,7 +73,13 @@ export type Datum = object & {
   [key: string]: any;
 };
 
-export type NumberFormat = 'currency' | 'shortCurrency' | 'shortNumber' | 'standardNumber' | 'percentage' | string;
+export type NumberFormat =
+  | 'currency'
+  | 'shortCurrency'
+  | 'shortNumber'
+  | 'standardNumber'
+  | 'percentage'
+  | (string & NonNullable<unknown>);
 export type Orientation = 'vertical' | 'horizontal';
 export type Position = 'left' | 'right' | 'top' | 'bottom';
 export type ScaleType = 'linear' | 'point' | 'time' | 'band';
