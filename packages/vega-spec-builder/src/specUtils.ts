7fb5ee0eb54d55e54568e7469002319287e28f95
Tanu Garg <tanugarg86@gmail.com>
2026-07-31 12:00:34 -0600

    fix: update type again

--- packages/vega-spec-builder/src/specUtils.ts
+++ packages/vega-spec-builder/src/specUtils.ts
@@ -281,7 +281,8 @@ export const getDimensionField = (dimension: string, scaleType?: ScaleType) => {
  * @param numberFormat
  * @returns
  */
-export const getD3FormatSpecifierFromNumberFormat = (numberFormat: NumberFormat | string): string => {  switch (numberFormat) {
+export const getD3FormatSpecifierFromNumberFormat = (numberFormat: NumberFormat): string => {
+  switch (numberFormat) {
     case 'currency':
       return '$,.2f'; // currency format
     case 'standardNumber':
