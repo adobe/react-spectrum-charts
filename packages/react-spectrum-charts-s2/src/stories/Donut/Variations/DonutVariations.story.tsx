9ea90711bbfa2ecbf96452f5ecbfdf7e927cae95
Maddi personal <madeline.laurel.luke@gmail.com>
2026-09-23 16:31:46 -0600

    chore: keep dashboard hidden

--- packages/react-spectrum-charts-s2/src/stories/Donut/Variations/DonutVariations.story.tsx
+++ packages/react-spectrum-charts-s2/src/stories/Donut/Variations/DonutVariations.story.tsx
@@ -52,7 +52,7 @@ export default {
     controls: { disable: true },
     layout: 'fullscreen',
   },
-  // tags: ['hidden']
+  tags: ['hidden']
 };
 
 const alternateFieldData = basicDonutData.map(({ browser, count }) => ({
